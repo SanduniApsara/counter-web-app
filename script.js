e1ed41877444f381ba2936215af4ba226f6f9387
Sanduni Apsara <sandu8098@gmail.com>
2026-04-29 14:20:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+(function(){
+
+    const buttons = document.querySelectorAll(".btn-counter");
+    let count = 0;
+
+    const counter = document.querySelector(".counter-machine");
+    const countercontainer = document.querySelector(".counter");
+
+    buttons.forEach(function(button){
+        button.addEventListener('click',function(){
+
+            if (button.classList.contains("decrease-btn")){
+                count--;
+            }else if(button.classList.contains("increase-btn")){
+                count++;
+
+            }
+            counter.textContent = count;
+
+            if(count > 0){
+                countercontainer.classList.remove("negative");
+                countercontainer.classList.add("positive");
+
+            }else if(count < 0){
+                countercontainer.classList.remove("positive");
+                countercontainer.classList.add("negative");
+
+            }
+
+    });
+
+    });
+
+})();
